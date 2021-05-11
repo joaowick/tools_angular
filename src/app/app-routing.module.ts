25ffe16f8c59a352d422f3ed8c4c6c780a2edb8a
Joao <joaotorrez14@gmail.com>
2021-05-11 16:54:30 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ToolsComponent } from './components/tools/tools.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ToolsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolsComponent } from './components/tools/tools.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolsComponent
   ],
   imports: [
     BrowserModule,
